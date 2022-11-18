89d0583417d30ec9bc57596bcaa1c05a9ec09536
Peerapat <85300665+peerapatsiriai@users.noreply.github.com>
2022-11-18 13:30:44 +0700

    delete pass

--- connection/connect.js
+++ connection/connect.js
@@ -2,10 +2,10 @@ const mysql = require('mysql')
 const util = require('util')
 // My SQL Connection
 const connection = mysql.createConnection({
-    host:"qz8si2yulh3i7gl3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
-    user:"tlgk8ei2m22o7gmz",
-    password:"lfn99tpoldn8owb1",
-    database:"emg77eshsupxjdc8",
+    host:"",
+    user:"",
+    password:"",
+    database:"",
     port:"3306"
 })
 
